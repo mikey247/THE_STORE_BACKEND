29838d3c59b97efbeb39fcf4d40fc5950e553c66
mikey247 <michaeljunior794@gmail.com>
2022-08-09 11:43:29 +0100

    cors

--- index.js
+++ index.js
@@ -20,23 +20,23 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://localhost:3000",
-      "https://127.0.0.1:3000",
-      "https://127.0.0.1:5000",
-      "https://checkout.paystack.com",
-      "https://thee-store.netlify.app",
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "http://127.0.0.1:5000",
+      "http://checkout.paystack.com",
+      "http://thee-store.netlify.app",
     ],
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
